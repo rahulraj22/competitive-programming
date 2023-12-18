69c55cd35da3f5acf8b97feb10894304d7900f03
Rahul Raj <rahulrajsjs22@gmail.com>
2023-12-18 22:18:11 +0530

    minor update

--- fenwick-tree.cpp
+++ fenwick-tree.cpp
@@ -3,7 +3,7 @@ int tree[N];  // Binary Tree is represented in array
 int a[N];
 int n; // size of the array/nums
 
-void update(int i, int x){
+void update(int i, int x){ // NOTE: i index is 1 based indexing 
     while(i <= n){
         tree[i] += x;
         i += (i & (-i));
