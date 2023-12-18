1606c35ef0177e75dcb51642d9849f84b1dc6f7e
Rahul Raj <rahulrajsjs22@gmail.com>
2023-12-18 22:16:25 +0530

    fenwick-tree.cpp

---
+++ fenwick-tree.cpp
@@ -0,0 +1,20 @@
+const int N = 1e5 + 10;
+int tree[N];  // Binary Tree is represented in array
+int a[N];
+int n; // size of the array/nums
+
+void update(int i, int x){
+    while(i <= n){
+        tree[i] += x;
+        i += (i & (-i));
+    }
+}
+int getSum(int i){
+    // to get sum till ith index
+    long long sum = 0;
+    while(i > 0){
+        sum += tree[i];
+        i -= (i & (-i));
+    }
+    return sum;
+}
